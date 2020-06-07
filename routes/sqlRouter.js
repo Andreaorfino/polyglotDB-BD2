cb281533cab66832144458de509c1865ee9881f1
Andreaorfino <sharswood.ap@gmail.com>
2020-06-07 23:46:28 +0200

    server fix1

--- routes/sqlRouter.js
+++ routes/sqlRouter.js
@@ -5,6 +5,7 @@ const db = require('../DB/SQL/connector');
 
 router.get('/', function(req, res, next) {
     db.query('SELECT * FROM public.fatture', (err, result) => {
+        console.log(err);
         res.send(result.rows);
 
         
